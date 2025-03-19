018a57c2fbb048d4c45adf9a29e9f36f0d8da5d8
Balabharathi Jayaraman <45947093+iambalabharathi@users.noreply.github.com>
2025-03-19 15:04:10 +0100

    Update mobile-automation-utilities.sh

--- mobile-automation-utilities.sh
+++ mobile-automation-utilities.sh
@@ -1,3 +1,11 @@
+function get-ipa-app-version() {
+    unzip -p "$1" 'Payload/*.app/Info.plist' | strings | awk -F'[<>]' '/CFBundleShortVersionString/{getline; print $3}'
+}
+
+function get-simulator-app-version() {
+    /usr/libexec/PlistBuddy -c 'Print CFBundleShortVersionString' '$1/Info.plist'
+}
+
 function list-connected-iphones-info() {
     printf "%-40s %-25s %-10s\n" "UDID" "Device Name" "iOS Version"
     printf "%-40s %-25s %-10s\n" "---------------------------------------------------------------------------------"
